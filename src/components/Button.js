de60676e754094add1a28332e3efcd578402a896
Eoin Gaughran <eoinbgaughran@gmail.com>
2021-05-07 19:05:23 +0200

    Hero Section w/ pages

--- src/components/Button.js
+++ src/components/Button.js
@@ -22,7 +22,7 @@ export const Button = ({
         : SIZES[0];
 
     return (
-        <Link to='/sign-up' classNames='btn-mobile'>
+        <Link to='/sign-up' className='btn-mobile'>
             <button className={'btn ${checkButtonStyle} ${checkButtonSize}'}
                 onClick={onClick}
                 type={type}
