04d45000fdf8e06f98b2eb915d7453cf99bad59e
Eoin Gaughran <eoinbgaughran@gmail.com>
2021-05-13 00:41:38 +0200

    Pages w/ subURL

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import { BrowserRouter as Router, Switch, Route } from
   'react-router-dom';
 import './App.css';
 import Home from './components/Pages/Home';
+import Services from './components/Pages/Services';
+import Products from './components/Pages/Products';
+import SignUp from './components/Pages/SignUp';
 
 function App() {
   return (
@@ -11,8 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component=
-            {Home} />
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+          <Route path='/sign-up' component={SignUp} />
         </Switch>
       </Router>
     </>
